7718cc70915ba55a95c6877d40cdcc9a379dd353
Yufan Lu <Yufan.Lu@campus.lmu.de>
2023-08-01 09:22:27 +0800

    test

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,5 @@
 //Listen detail page info
 
-// 使用选择器获取包含产品名称的元素
+// 使用选择器获取包含产品名称的元素 测试
 let productName = document.querySelector('.tb-main-title').textContent;
 chrome.runtime.sendMessage({product: productName});
