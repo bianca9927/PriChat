3ee61bc52e84ad0d0f464de780996aaf5d6a0b10
bianca9927 <1962163657@qq.com>
2023-08-03 16:39:15 +0800

    upload in github

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-// Listen msg from content
+// Listen msg from content--initial version
 
 //chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   //    if (request.product) {
@@ -8,21 +8,19 @@
       
 //});
 
-let port;
 
+//use long connect
+let port;
 chrome.runtime.onConnect.addListener(function (newPort) {
-  
     port = newPort;
     console.log("Connected to Popup.");
   }
 );
 
-// 监听来自 content.js 的消息
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.product) {
     console.log('Product Name: ' + request.product);
     if (port) {
-      // 向 Popup 发送消息
       port.postMessage({product_forpopup: request.product });
     }
   }
