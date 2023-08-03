1bf10f777b64cfcd721f3df5e41d2d63a8140c6e
Yufan Lu <Yufan.Lu@campus.lmu.de>
2023-08-03 16:21:39 +0800

    add long connect

--- background.js
+++ background.js
@@ -1,11 +1,32 @@
 // Listen msg from content
 
-    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-      if (request.product) {
-        // 在插件弹窗中显示产品名称
-        console.log('Product Name: ' + request.product);
-      }
+//chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  //    if (request.product) {
+    //    console.log('Product Name: ' + request.product);
+      //  chrome.runtime.sendMessage({product: request.product });
+      //}
+      
+//});
+
+let port;
+
+chrome.runtime.onConnect.addListener(function (newPort) {
+  
+    port = newPort;
+    console.log("Connected to Popup.");
+  }
+);
+
+// 监听来自 content.js 的消息
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.product) {
+    console.log('Product Name: ' + request.product);
+    if (port) {
+      // 向 Popup 发送消息
+      port.postMessage({product_forpopup: request.product });
+    }
+  }
 });
 
-  
+
   
--- popup/popup.js
+++ popup/popup.js
@@ -1,13 +1,19 @@
-// 接收来自 content script 的消息
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.action === "showProductName") {
-      // 获取产品名称，并设置为popup.html中展示元素的文本内容
-      const productNameDiv = document.getElementById("tb-main-title");
-      if (productNameDiv) {
-        productNameDiv.innerText = request.productName;
+// Listen msg from Background
+window.onload = function() {
+  // 建立长连接
+  const port = chrome.runtime.connect({ name: "popupConnection" });
 
+
+port.onConnect.addListener(function() {
+  console.log("Connected to Background.");
+
+  port.onMessage.addListener(function(request) {
+    console.log("Received message from Background:", request);
+      if (request.product_forpopup) {
+        console.log('Product Name in Popup: ' + request.product_forpopup);
+        document.getElementById("product-name").innerText = request.product_forpopup;
       }
-      
-    }
+    
   });
-  
+});
+};
